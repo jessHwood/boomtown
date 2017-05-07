abcec535c314bc765a4972a91520a1f27361bf61
ntutchton <ntutchton@gmail.com>
2017-05-07 12:08:01 -0600

    working DB from hardcode

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,1 +1,41 @@
 var DB = require("../models").models;
+
+
+var fakeResults = {
+	question1: 'female',
+	question2: 'lorem',
+	question3: "Chtulhu R'lyeh f'taguhn",
+	question4: "Sunday",
+	question5: "4,000",
+	question6: "Sci-fi and fanstasy",
+	question7: "The King in the North"
+};
+
+var ownerCreate = function(){
+	return DB.Owner.create({
+		name: 'Kathy',
+		shopName: 'HipsterBooks',
+		email: 'kathy@hipsterbooks.com',
+	});
+};
+
+var resultCreate = function(ownerId){
+	return DB.Result.create({
+		question1: 'female',
+		question2: 'lorem',
+		question3: "Chtulhu R'lyeh f'taguhn",
+		question4: "Sunday",
+		question5: "4,000",
+		question6: "Sci-fi and fanstasy",
+		question7: "The King in the North",
+		ownerId : ownerId
+	});
+};
+
+ownerCreate()
+	.then(function(owner){
+		resultCreate(owner.id)
+			.then(function(){
+				process.exit();
+			});
+	});
