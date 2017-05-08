fb6f697d720faf11696aaf0a6351edcaaaa5af3a
jessHwood <jhorntvedt@gmail.com>
2017-05-07 18:27:13 -0600

    edits wording on the survey qs

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 //Connect
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://ntutchton@localhost:5432/boomtown');
+var sequelize = new Sequelize('postgres://jesswood@localhost:5432/boomtown');
 
 //Export models and Sequelize for seed and dbSetup
 module.exports.Sequelize = Sequelize;
